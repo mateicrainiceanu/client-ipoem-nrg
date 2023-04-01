0ee830de3670ae9d2e1a17c0215608acf27d91d1
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2023-04-02 00:05:02 +0300

    React-separated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Header from "../components/Home/Header";
+import About from '../components/Home/About';
+
+
+const Home = () => {
+
+    return (
+        <div>
+            <Header />
+            <About />
+        </div>
+    );
+};
+
+export default Home;
